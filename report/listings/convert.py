5cdbb2283ca045827e23447c0b621784124da461
Jeppe BLicher Tarp <raider.j@gmail.com>
2012-12-13 13:33:35 +0100

    Added comments to listings

--- report/listings/convert.py
+++ report/listings/convert.py
@@ -1,25 +1,38 @@
+''' Python Code '''
+
+# Class containing raw Kinect input
 class KinectData(object): 
-	#...
+	# Has properties h, v, depth
+	# and "static" members hres, vres, hfield, vfield
+	
+	# Convert depth to mm using formula from
+	#   http://openkinect.org/wiki/Imaging_Information
 	def depth_mm(self):
 		return 0.1236 * math.tan(self.depth / 2843.5 + 1.1863) * 1000.0
 
+	# Calculate horizontal degrees to point from
+	#   pixel position, using image resolution and field of view.
 	def h_deg(self):
 		return self.h / KinectData.hres * KinectData.hfield
 
+	# Same as above, but vertical
 	def v_deg(self):
 		return self.v / KinectData.vres * KinectData.vfield
 
+# Class containing a vector with 3 elements
 class Vector3(object):
-	#...
+	# Has properties x, y, z
+	
+	# Transform a point from KinectData to Vector3
 	def _kinect(self, kinect_data):
-		if type(kinect_data) != KinectData:
-			raise TypeError('Expected a KinectData instance')
-
+	
+		# Get distance to point, from (0, 0, 0)
 		depth = kinect_data.depth_mm()
 
+		# Use the law of sines to find x and y
 		self.x = int(math.sin(math.radians(kinect_data.h_deg())) * (depth / math.sin(math.radians(90.0))))
 		self.y = int(-(math.sin(math.radians(kinect_data.v_deg())) * (depth / math.sin(math.radians(90.0)))))
 
+		# Use Pythagoras' theorem to find z
 		dist2 = math.sqrt(self.x * self.x + self.y * self.y)
-
 		self.z = int(math.sqrt(depth * depth - dist2 * dist2))
