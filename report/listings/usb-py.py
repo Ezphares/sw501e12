77d4605af4fe8df2bf03069d1037fa470c249652
Simon Jensen <mail@simonjensen.net>
2012-12-18 13:40:26 +0100

    Partly changed implement-usb

--- report/listings/usb-py.py
+++ report/listings/usb-py.py
@@ -1,21 +1,15 @@
-""" Converts each of the position coordinates x, y and z as well as the speed vector x, y and z coordinates to an array of chars. """
 def convert(self, number):
-#If the number is negative, the sign bit will be set to 1, otherwise it will be set to 0.
     if number < 0:
         sign = 1
         number = -number
     else:
         sign = 0
-#If the number is larger than an int, the data is not valid, and the array will be zeroed.
     if(number >= 65536):
         return [0, 0, 0]
-#The int number is converted into an array of chars so that it can be sent over USB.
     else:
         return [sign, int(number / 256), int(number % 256)]
         
-
 def send_data(self, pos_x, pos_y, pos_z, speed_x, speed_y, speed_z):
-#Every input is an array converted from an int, they are all added to one array that can be sent over USB.
     data = []
     data.extend(self.convert(pos_x))
     data.extend(self.convert(pos_y))
